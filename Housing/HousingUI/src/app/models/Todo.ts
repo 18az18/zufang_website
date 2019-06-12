6f4b51df40be909cb0c55b1f3c5bb4d0fbe2be9c
TianJi Liu <tianjiliu@MacBook-Air-2.local>
2019-06-12 23:46:45 +0800

    init

---
+++ Housing/HousingUI/src/app/models/Todo.ts
@@ -0,0 +1,13 @@
+export class Todo{
+    id: number;
+    name?:string; // name is optional with the ?
+}
+
+ 
+// import { Todo } from ... to use it in other component file
+// import {Input} from angular/core to grab the passed in attribute from other html 
+// @Input() attribute_name:type;
+// [ngClass] to set the class attributes to a selector
+// ng g s services/xx to generate services
+// ng g c components/xx to generate component
+// import { Observable } from 'rxjs'
