de160439db67c865a8a0e428343a3c56b3722ea2
TianJi Liu <tianjiliu@MacBook-Air-2.local>
2019-07-02 20:17:36 +0800

    login-singup

--- Housing/HousingUI/src/app/components/pages/signup/signup.component.ts
+++ Housing/HousingUI/src/app/components/pages/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -11,21 +12,29 @@ export class SignupComponent implements OnInit {
 
   signupForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) { }
+  constructor(private fb: FormBuilder, private router: Router, private userService: UserService) { }
 
   ngOnInit() {
     this.signupForm = this.fb.group({
       userName: ['', Validators.required],
-      password: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
       email: ['', [Validators.required, Validators.email]],
-      phoneNumber: ['']
+      phoneNumber: ['', Validators.required]
     });
-
     }
 
-
   onSubmit() {
-    console.log('submitted');
+    const username = this.signupForm.get('userName').value;
+    const password = this.signupForm.get('password').value;
+    const email = this.signupForm.get('email').value;
+    const phoneNumber = this.signupForm.get('phoneNumber').value;
+    console.log('submitted username:' + username + '/' + 'password:' + password +  '/' + 'email:' + email);
+    console.log('Phone number: ' + phoneNumber);
+    this.userService.signup(username, password, email, phoneNumber).subscribe(res => {
+      console.log(res);
+    }, err => {
+      console.log(err);
+    });
   }
 
   signin() {
